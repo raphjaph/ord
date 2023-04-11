b16c1db28c22bff117f74aa816be007a70b8d0f6
José A.P <jose@clovrlabs.com>
2023-04-11 13:18:12 +0200

    feat(api.rs): add panic hook to panic on any async or thread panic

--- src/subcommand/api.rs
+++ src/subcommand/api.rs
@@ -1,12 +1,17 @@
 use super::*;
-use anyhow::Ok;
-use tokio::runtime::Runtime;
 use crate::rpc::start_grpc_server;
+use anyhow::Ok;
+use std::panic;
+use tokio::runtime::Runtime;
+
 pub(crate) fn run(options: Options) -> Result {
+
+  //Hook to make sure that the program panics on any async or thread panic
+  panic::set_hook(Box::new(|info| {
+    panic!("Panic hooked, let's panic")
+  }));
 
   let result = Runtime::new().unwrap().block_on(start_grpc_server(options));
 
   Ok(())
-
 }
-
